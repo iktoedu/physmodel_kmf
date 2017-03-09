c982016b353534fbf789eae21d7d6880b3dc6054
Maxim Polishchuck <paulmaxwell@users.noreply.github.com>
2017-03-09 22:54:18 +0200

    added AtomGridIterator

---
+++ simple_2d/simple_2d_atom_grid_iterator.cpp
@@ -0,0 +1,46 @@
+#include "simple_2d_atom_grid_iterator.h"
+
+namespace Simple2D {
+
+AtomGridIterator::AtomGridIterator(btAtomValue **field, btPositionValue sizeX, btPositionValue sizeY)
+    : mvpField(field), mvSizeX(sizeX), mvSizeY(sizeY)
+{
+    mvPositionX = 0;
+    mvPositionY = 0;
+}
+
+AtomGridIterator::AtomGridIterator(const AtomGridIterator &iterator)
+    : mvpField(iterator.mvpField), mvSizeX(iterator.mvSizeX), mvSizeY(iterator.mvSizeY), mvPositionX(iterator.mvPositionX), mvPositionY(iterator.mvPositionY)
+{
+}
+
+AtomGridIterator &AtomGridIterator::operator ++()
+{
+    ++mvPositionX;
+    if (mvPositionX >= mvSizeX) {
+        mvPositionX = 0;
+        ++mvPositionY;
+    }
+
+    return *this;
+}
+
+AtomGridIterator AtomGridIterator::operator ++(int)
+{
+    AtomGridIterator current(*this);
+    operator ++();
+
+    return current;
+}
+
+atomReference2D AtomGridIterator::operator *()
+{
+    atomReference2D ref;
+    ref.field = mvpField;
+    ref.x = mvPositionX;
+    ref.y = mvPositionY;
+
+    return ref;
+}
+
+} // namespace Simple2D
---
+++ simple_2d/simple_2d_atom_grid_iterator.h
@@ -0,0 +1,35 @@
+#ifndef SIMPLE_2D_ATOM_GRID_ITERATOR_H
+#define SIMPLE_2D_ATOM_GRID_ITERATOR_H
+
+#include <iterator>
+#include "core_2d/core_2d_atom_reference.h"
+
+namespace Simple2D {
+
+class AtomGridIterator : public std::iterator<std::input_iterator_tag, atomReference2D>
+{
+public:
+    AtomGridIterator(btAtomValue **, btPositionValue, btPositionValue);
+    AtomGridIterator(const AtomGridIterator &);
+
+    AtomGridIterator &operator ++();
+    AtomGridIterator operator ++(int);
+
+//    bool operator==(const AtomGridIterator &);
+//    bool operator!=(const AtomGridIterator &);
+
+    atomReference2D operator *();
+
+private:
+    btAtomValue **mvpField;
+
+    btPositionValue mvSizeX;
+    btPositionValue mvSizeY;
+
+    btPositionValue mvPositionX;
+    btPositionValue mvPositionY;
+};
+
+} // namespace Simple2D
+
+#endif // SIMPLE_2D_ATOM_GRID_ITERATOR_H
